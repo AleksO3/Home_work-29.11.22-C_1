ea4cd23e3dd86bd8209f37ab72bc3bd4e921859d
AleksO3 <118824777+AleksO3@users.noreply.github.com>
2022-12-09 16:48:18 +0400

    добавлена 20 задача из 3-го практического задания

---
+++ 20(3)/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 20: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+Console.Clear();
+Console.WriteLine("Введите координаты точки A, значения x и y");
+int Xa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки A");
+int Ya = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B, значения x и y");
+int Xb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки B");
+int Yb = Convert.ToInt32(Console.ReadLine());
+
+if (Xa == Xb && Ya == Yb)
+{
+    Console.WriteLine("Расстояние = 0; введите разные значения координат");
+}
+else
+{
+    double Res = Math.Sqrt(Math.Pow(Xb - Xa,2) + Math.Pow(Yb - Ya,2));
+    Console.WriteLine($"Длина отрезка AB равна {Res}");
+}
--- 20(3)/obj/Debug/net6.0/20(3).AssemblyInfo.cs
+++ 20(3)/obj/Debug/net6.0/20(3).AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("18-3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("20(3)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("18-3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("18-3")]
+[assembly: System.Reflection.AssemblyProductAttribute("20(3)")]
+[assembly: System.Reflection.AssemblyTitleAttribute("20(3)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
