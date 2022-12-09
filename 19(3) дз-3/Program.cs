1b48b13015836832610d6d59697fbb7fc77c78f5
AleksO3 <118824777+AleksO3@users.noreply.github.com>
2022-12-09 20:44:34 +0400

    Arrays доп.материаля

--- 19(3) дз-3/Program.cs
+++ 19(3) дз-3/Program.cs
@@ -3,6 +3,10 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
 Console.Clear();
 Console.WriteLine("Введите пятизначное число или около того");
 int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(string.Concat($"{a}".Reverse()));
+int result = Int32.Parse(a);
+Console.WriteLine(result);
--- Доп.материал Arrays/Program.cs
+++ Доп.материал Arrays/Program.cs
@@ -1,2 +1,5 @@
 ﻿Console.Clear();
 // int[] myIntArray = new int[5] { 1, 2, 3, 4, 5 };
+
+// int result = Int32.Parse(input);
+// Console.WriteLine(result); Преобразовать строку в Int
