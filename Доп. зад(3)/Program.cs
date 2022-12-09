500eb64446c4e46369f147d45e25f3ca84056d12
AleksO3 <118824777+AleksO3@users.noreply.github.com>
2022-12-09 19:18:51 +0400

    Доп. задание из 3-го практического задания

--- Доп. зад(3)/Program.cs
+++ Доп. зад(3)/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Доп. задание
 // Написать метод для разворота массива, не используя память под дополнительный массив
 Console.Clear();
-int[] array = { 3, 7, 9 };
-for (int i = array.Length - 1; i--)
+int[] array = {3, 7, 9, 5,11};
+for (int i = array.Length - 1; i >=0; i--)
 {
     Console.Write(array[i]);
 }
