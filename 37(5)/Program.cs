89a0309153eeae0e3ef12e1adae1452909c9850e
AleksO3 <118824777+AleksO3@users.noreply.github.com>
2022-12-13 23:13:40 +0400

    class-work(37)задача

--- 37(5)/Program.cs
+++ 37(5)/Program.cs
@@ -2,4 +2,31 @@
 // [1 2 3 4 5] -> 5 8 3
 // [6 7 3 6] -> 36 21
 // int[] array = {1,2,3,4,5};
+
+// (array.Length + 1)/ 2
+// array.Length  / 2 + array.Length % 2
+
 Console.Clear();
+int[] array = { 55, 2, 3, 4, 5 };
+
+int[] array2 = new int[array.Length  / 2 + array.Length % 2];
+for(int i = 0; i < array2.Length; i++)
+{
+    array2[i] = array[i]*array[array.Length-1-i];
+}
+Console.WriteLine(string.Join(", ", array2));
+
+//// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+// if вынести за цикл
+
+// int[] array = {1,2,3,4,5};
+
+// int[] array2 = new int [array.Length  / 2 + array.Length % 2];
+
+// for (int i = 0; i < array2.Length; i++)
+// {
+//     array2[i]= array[i]*array[array.Length-1-i];
+// }
+// Console.WriteLine(string.Join(", " ,  array2));
