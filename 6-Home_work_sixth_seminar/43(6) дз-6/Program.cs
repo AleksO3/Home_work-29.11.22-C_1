5dd8ac7de3ba902c42fa33a92aa591196cdb1e89
AleksO3 <118824777+AleksO3@users.noreply.github.com>
2022-12-23 20:44:15 +0400

    home-works-41,43-tasks 23.12.22s

--- 6-Home_work_sixth_seminar/43(6) дз-6/Program.cs
+++ 6-Home_work_sixth_seminar/43(6) дз-6/Program.cs
@@ -3,7 +3,7 @@
 // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 Console.Clear();
-int ReadInt(string message)                         // Создали метод для вывода на экран и получения числа (c 7-11 строку)
+int ReadInt(string message)                         // Создали метод для вывода на экран и получения числа
 {
     Console.WriteLine(message);
     return Convert.ToInt32(Console.ReadLine());     // От пользователя ответ прочитали, сконвертировали и сразу  отдали
