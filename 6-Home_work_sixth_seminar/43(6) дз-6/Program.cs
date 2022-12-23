1fb1a6905eafc54ecc6667536ba61bb41aae588c
AleksO3 <118824777+AleksO3@users.noreply.github.com>
2022-12-23 20:47:08 +0400

    home-works-41,43-tasks 23.12.22 end!!!

--- 6-Home_work_sixth_seminar/43(6) дз-6/Program.cs
+++ 6-Home_work_sixth_seminar/43(6) дз-6/Program.cs
@@ -3,11 +3,11 @@
 // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 Console.Clear();
-int ReadInt(string message)                         // Создали метод для вывода на экран и получения числа
-{
-    Console.WriteLine(message);
-    return Convert.ToInt32(Console.ReadLine());     // От пользователя ответ прочитали, сконвертировали и сразу  отдали
-}
+// int ReadInt(string message)                         // Создали метод для вывода на экран и получения числа
+// {
+//     Console.WriteLine(message);
+//     return Convert.ToInt32(Console.ReadLine());     // От пользователя ответ прочитали, сконвертировали и сразу  отдали
+// }
 
 double b1 = ReadInt("Введите число: ");
 double k1 = ReadInt("Введите число: ");
@@ -23,7 +23,5 @@ else
 {
     double x = (b1 - b2) / (k2 - k1);
     double y = k2 * x + b2;
-
-
     Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
 }
