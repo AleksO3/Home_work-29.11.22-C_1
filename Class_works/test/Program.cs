4e2a32f0bbee540fb5108f6d1961f556575bffa2
AleksO3 <118824777+AleksO3@users.noreply.github.com>
2022-12-25 23:18:48 +0400

    class-works-25.12.22

--- Class_works/test/Program.cs
+++ Class_works/test/Program.cs
@@ -78,4 +78,37 @@
 // 9 4 2
 // 2 2 6
 // 3 4 7
-int[,] array = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+// int[,] array = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+// 67
+// Console.WriteLine("Введите число");
+// int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+// int Line(int sum)
+// {
+//     if (sum == 0) return 0;
+//     else
+//     {
+//         sum = sum % 10 + Line(sum / 10);
+//     }
+//     return sum;
+// }
+// Console.WriteLine(Line(num));
+
+// 69
+// Console.WriteLine("Введите основание и степень");
+// int osn = Convert.ToInt32(Console.ReadLine());
+// int step = Convert.ToInt32(Console.ReadLine());
+// int Stepen(int n, int m)
+// {
+//     if (m == 0) return 1;
+//     n += Stepen(n, m - 1);
+//     return n;
+// }
+// Console.WriteLine(Stepen(osn, step));
+
