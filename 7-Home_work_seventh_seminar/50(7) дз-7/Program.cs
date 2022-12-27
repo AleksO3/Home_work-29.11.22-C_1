c7d59cd834e491d579acde78bdffa31bf50ad771
AleksO3 <118824777+AleksO3@users.noreply.github.com>
2022-12-27 21:37:09 +0400

    home-work-47-50-52-end!!!

--- 7-Home_work_seventh_seminar/50(7) дз-7/Program.cs
+++ 7-Home_work_seventh_seminar/50(7) дз-7/Program.cs
@@ -33,7 +33,7 @@ for (int i = 0; i < array.GetLength(0); i++)
 Console.WriteLine();
 int x = ReadInt("Введите координаты индекса строки");
 int y = ReadInt("Введите координаты индекса столбца");
-if (x > m || y > n || x < 0 || y < 0 || x > m && y > n || x < 0 && y < 0 || x > m && y < 0 || x < 0 && y > n)
+if (x > m || y > n || x < 0 || y < 0)
     Console.WriteLine("позиции элемента не верно указаны, такого элемента нет");
 else
 {
